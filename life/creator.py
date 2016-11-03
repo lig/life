86f70bc66ecf22c02999467528868f5f46c97b28
Serge Matveenko <s@matveenko.ru>
2016-11-04 02:21:38 +0300

    Fix logic.

--- life/creator.py
+++ life/creator.py
@@ -15,9 +15,7 @@ class Creator:
         self._window.push_handlers(self)
         self.width = window.width // CELL_SIZE
         self.height = window.height // CELL_SIZE
-        self.field = numpy.empty(
-            shape=[self.width, self.height],
-            dtype=int)
+        self.field = self._get_empty_field()
         self._draw_queue = deque()
         for y in range(1, self.height - 1):
             for x in range(1, self.width - 1):
@@ -39,18 +37,21 @@ class Creator:
                 ('c3B', (0, 255 * value, 0) * 4))
 
     def _cycle(self, dt):
-        new_field = numpy.empty(
-            shape=[self.width, self.height],
-            dtype='int8')
+        new_field = self._get_empty_field()
         for y in range(1, self.height - 1):
             for x in range(1, self.width - 1):
-                mass = sum(sum(self.field[x - 1:x + 2:2, y - 1:y + 2:2]))
                 value = self.field[x, y]
-                new_value = value ^ int(
-                    not value and mass in BORN or
-                    value and mass not in SURVIVE)
+                mass = sum(sum(self.field[x - 1:x + 2, y - 1:y + 2])) - value
+                new_value = value ^ (
+                    ~ value & (mass in BORN) |
+                    value & ~ (mass in SURVIVE))
                 new_field[x, y] = new_value
                 if new_value ^ value:
                     self._draw_queue.append((x, y, new_value))
         self.field = new_field
         clock.schedule_once(self._cycle, 0)
+
+    def _get_empty_field(self):
+        return numpy.zeros(
+            shape=[self.width, self.height],
+            dtype='int8')
