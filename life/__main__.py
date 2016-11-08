5a2b287fdcffe604ee727ff4113472ea8c697fad
Serge Matveenko <s@matveenko.ru>
2016-11-08 04:42:31 +0300

    Add fps display.

--- life/__main__.py
+++ life/__main__.py
@@ -1,6 +1,6 @@
 import pyglet
 
-from life import WIDTH, HEIGHT, CELL_SIZE
+from life import WIDTH, HEIGHT, CELL_SIZE, DISPLAY_FPS
 from life.creator import Creator
 from life.view import Field
 
@@ -9,12 +9,15 @@ creator = Creator(width=WIDTH, height=HEIGHT)
 field = Field(field_creator=creator, cell_size=CELL_SIZE)
 
 window = pyglet.window.Window(width=field.width, height=field.height)
+fps_display = pyglet.clock.ClockDisplay()
 
 
 @window.event
 def on_draw():
     window.clear()
     field.draw()
+    if DISPLAY_FPS:
+        fps_display.draw()
 
 
 creator.start()
